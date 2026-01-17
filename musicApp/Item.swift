e6c35d395978e284faade3ee2304f3e2306243f8
TP <ploso@TPs-MacBook-Air.local>
2026-01-17 16:44:30 -0600

    initial xcode setup

---
+++ musicApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  musicApp
+//
+//  Created by TP on 1/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
