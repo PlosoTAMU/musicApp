9e37e46039df72bb484b5e97f903212c2015db87
PlosoTAMU <truman.porter@tamu.edu>
2026-01-17 20:04:55 -0600

    hello world

--- musicApp/ContentView.swift
+++ musicApp/ContentView.swift
@@ -1,26 +1,24 @@
-//
-//  ContentView.swift
-//  musicApp
-//
-//  Created by TP on 1/17/26.
-//
-
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
     var body: some View {
         ZStack {
+            // Fills the entire background with white, including safe areas
             Color.white
                 .ignoresSafeArea()
 
             Text("Hello, World!")
                 .font(.largeTitle)
-                .foregroundStyle(.black)
+                // .foregroundColor is the classic modifier, fully compatible with iOS 16
+                .foregroundColor(.black)
         }
     }
 }
 
-#Preview {
-    ContentView()
+// This is the older, but still perfectly valid, way to create a preview
+// It's required when your code needs to be compatible with pre-iOS 17 targets
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
