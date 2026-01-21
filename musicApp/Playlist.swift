5994f5dd059fd7c17c071bdc2ac801044907f31b
TP <ploso@TPs-MacBook-Air.local>
2026-01-20 20:57:28 -0600

    Revert "persstientse :wq  playlists"
    This reverts commit 2e25b1aa86bac1dd80b27eb1066b090bc5386cbc.

--- musicApp/Playlist.swift
+++ musicApp/Playlist.swift
@@ -1,13 +1,7 @@
 import Foundation
 
-struct Playlist: Identifiable, Codable {
-    let id: UUID
+struct Playlist: Identifiable {
+    let id = UUID()
     let name: String
     var tracks: [Track]
-    
-    init(id: UUID = UUID(), name: String, tracks: [Track]) {
-        self.id = id
-        self.name = name
-        self.tracks = tracks
-    }
 }
